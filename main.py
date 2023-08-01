ad3eccf709565ee8870a540a3c392c8795d0472a
Orbasker <87074045+Orbasker@users.noreply.github.com>
2023-08-01 15:53:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -13,4 +13,4 @@ def hello_world():
     return {"message": "Hello World"}
 
 if __name__ == '__main__':
-    uvicorn.run(app, host="0.0.0.0", port=80)
+    uvicorn.run(app, host="0.0.0.0", port=60505)
