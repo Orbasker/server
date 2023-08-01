56301eabef6e5be6a00bd803d97f1dc40a54206c
Orbasker <87074045+Orbasker@users.noreply.github.com>
2023-08-01 14:29:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ app = FastAPI()
 def hello_world():
     return {"message": "Hello World"}
 
+@app.get("/test")
+hello_world()
+
 
 if __name__ == '__main__':
-    uvicorn.run(app, host="0.0.0.0", port=65050)
+    uvicorn.run(app, host="0.0.0.0", port=60505)
